bce51cab5de3fde5c0aa709c7acdc06ea5ad3513
DANIEL REMOQUILLO <remoquillodr@gmail.com>
2023-02-20 23:21:50 +0800

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Music Player',
       theme: ThemeData(
           appBarTheme: AppBarTheme(
               backgroundColor: MusicPlayerTheme().primaryAppBarColor)),
